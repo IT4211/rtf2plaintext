a530a46a490f38e02d99fccce291c4e57141d35e
IT4211 <rhdqor100@live.co.kr>
2017-01-13 05:13:56 +0900

    Update rtf2plaintext.py
    평문자열 파싱하는 부분 계속 이어서 구현 할 것!

--- rtf2plaintext.py
+++ rtf2plaintext.py
@@ -4,11 +4,13 @@
 """
 
 def bracket(input):
-    #
+    # Brace removal function
     stack = list()
     chunk = str()
     rtftext = input.read()
+    rtftext = rtftext.replace('\n', '')
 
+    # simple pushdown automata
     for _ch in rtftext:
         if len(stack) == 0 and _ch == '{':
             stack.append('G')
@@ -25,7 +27,52 @@ def bracket(input):
                 yield chunk
 
 
+def parse_plaintext(input):
+    chk_pt_tag = ['sectd', 'pard', 'plain', 'rtlch']
+    seek = 0
+    stack = list()
+    chunk = str()
+    pt_offset = str()
+
+    for _ch in input:
+        if len(stack) == 0 and _ch == '\\':
+            stack.append('S')
+
+        elif stack[-1] == 'S' and _ch.isalpha():
+            chunk += _ch
+            seek += 1
+
+        elif stack[-1] == 'S' and (_ch == '\\' or not _ch.isalpha()):
+            stack.pop()
+            if len(stack) == 0:
+                if chk_pt_tag[0] in chunk:
+                    continue
+                elif chk_pt_tag[1] in chunk:
+                    continue
+                elif chk_pt_tag[2] in chunk:
+                    continue
+                elif chk_pt_tag[3] in chunk:
+                    pt_offset = input[seek:]
+                    break
+
+    for _ch in pt_offset:
+        print "[d]", _ch
+
+
+
+
+
+
+
 if __name__=="__main__":
+    tmp = str()
     richtf = open('test.rtf', 'r')
+    plaintf = open('rtf2pt.txt', 'w')
     for tag_chunk in bracket(richtf):
-        print tag_chunk
+        tmp += tag_chunk
+        tmp = tmp.replace("\\rtlch", "\n\\rtlch")
+       #plaintf.write(tmp)
+        parse_plaintext(tmp)
+
+
+    #print file_meta_tag(tmp)
